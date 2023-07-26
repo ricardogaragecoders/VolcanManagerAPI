58b01ba32b86556a506f86301395eae40c309657
Ricardo Garage Coders <ricardo@garagecoders.net>
2023-07-26 11:24:23 -0600

    VM-129 Add config to db

--- volcanmanagerapi/settings.py
+++ volcanmanagerapi/settings.py
@@ -96,10 +96,7 @@ WSGI_APPLICATION = 'volcanmanagerapi.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
 }
 
 # Backend login
