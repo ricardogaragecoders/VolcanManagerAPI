79abc306ecb2130c5d06167778890e0e59451d09
Ricardo Garage Coders <ricardo@garagecoders.net>
2023-08-01 19:41:37 -0600

    VM-133 Install NewRelic

--- volcanmanagerapi/wsgi.py
+++ volcanmanagerapi/wsgi.py
@@ -6,6 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
+try:
+    import newrelic.agent
+    newrelic.agent.initialize('/opt/apps/config/newrelic.ini')
+    newrelic_exists = True
+except ImportError:
+    newrelic_exists = False
+
 
 import os
 
@@ -14,3 +21,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'volcanmanagerapi.settings')
 
 application = get_wsgi_application()
+
+if newrelic_exists:
+    application = newrelic.agent.WSGIApplicationWrapper(application)
